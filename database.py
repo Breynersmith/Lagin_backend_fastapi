baaaee818d6567e207f5fb0a7e041e1be89ad6af
breynersmith <breynersmithustariz@gmail.com>
2024-12-09 17:36:01 -0500

    primer commit

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# URL de la base de datos para conectar a SQLite
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db" 
+
+# Crea una instancia del motor de la base de datos
+# connect_args={"check_same_thread": False} es necesario para SQLite en aplicaciones multihilo
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Crea una fábrica de sesiones para interactuar con la base de datos
+# autocommit=False asegura que las transacciones no se confirmen automáticamente
+# autoflush=False evita que los cambios se envíen automáticamente a la base de datos
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Clase base para definir modelos de SQLAlchemy
+Base = declarative_base()
