b9f31c43d599c71e756726e0a441aa5677afd624
Breyner Ustariz <146795970+Breynersmith@users.noreply.github.com>
2024-12-10 15:21:18 -0500

    db posgsql agregada

--- database.py
+++ database.py
@@ -2,15 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import os
 
-database_dir = os.path.dirname(os.path.abspath(__file__))
 # URL de la base de datos para conectar a SQLite
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(database_dir, 'database.db')}" 
-
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://db_login_8pok_user:HihVbRsMtBzdehPfp6pBlTLnZrNztJXU@localhost:5432/db_login_8pok"
+)
 # Crea una instancia del motor de la base de datos
 # connect_args={"check_same_thread": False} es necesario para SQLite en aplicaciones multihilo
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Crea una fábrica de sesiones para interactuar con la base de datos
 # autocommit=False asegura que las transacciones no se confirmen automáticamente
